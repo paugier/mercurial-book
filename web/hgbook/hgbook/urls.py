cbb2f6845f489887aab867bfe1eeb61aca4ec053
Mathias De Maré <mathias.demare@gmail.com>
2015-05-30 20:18:42 +0200

    web: add home page

--- web/hgbook/hgbook/urls.py
+++ web/hgbook/hgbook/urls.py
@@ -3,6 +3,7 @@ import comments.feeds as feeds
 from django.contrib import admin
 from django.conf.urls import include, url
 from django.contrib.syndication.views import Feed
+from django.views.generic import TemplateView
 import settings
 
 admin.autodiscover()
@@ -18,6 +19,7 @@ urlpatterns = [
      {'feed_dict': feeds}),          
 
     # Only uncomment this for local testing without Apache.
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^html/(?P<path>.*)$', 'django.views.static.serve',
      {'document_root': os.path.join(settings.BASE_DIR, '..', '..', 'build', 'en', 'html')}),
     url(r'^support/(?P<path>.*)$', 'django.views.static.serve',
