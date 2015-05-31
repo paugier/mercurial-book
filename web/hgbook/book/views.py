dbc98a7d576b0c477d99cbe75397943046b6f3ed
Mathias De Maré <mathias.demare@gmail.com>
2015-05-31 17:56:40 +0200

    book: add autogenerated homepage

---
+++ web/hgbook/book/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+import glob, os, re
+from django.conf import settings
+
+chapter_re = re.compile(r'<(chapter|appendix|preface)\s+id="([^"]+)">')
+filename_re = re.compile(r'<\?dbhtml filename="([^"]+)"\?>')
+title_re = re.compile(r'<title>(.*)</title>')
+
+def index(request):
+    chapters = (sorted(glob.glob('../../en/ch*.xml')) +
+                sorted(glob.glob('../../en/app*.xml')))
+
+    ch = 0
+    app = 0
+    chaps = []
+    for c in chapters:
+        filename = None
+        title = None
+        chapid = None
+        chaptype = None
+        for line in open(c):
+            m = chapter_re.search(line)
+            if m:
+                chaptype, chapid = m.groups()
+            m = filename_re.search(line)
+            if m:
+                filename = m.group(1)
+            m = title_re.search(line)
+            if m:
+                title = m.group(1)
+            if filename and title and chapid:
+                if chaptype == 'appendix':
+                    num = chr(ord('A') + app)
+                    app += 1
+                else:
+                    num = ch
+                    ch += 1
+                date = os.popen('hg log -l1 --template "{date|isodate}" ' + c).read().split(None, 1)[0]
+                args = {
+                    'date': date,
+                    'feed': '/feeds/comments/%s' % chapid,
+                    'index': num,
+                    'url': filename,
+                    'title': title,
+                }
+                chaps.append(args)
+                break
+    return render(request, 'home.html', {'chapters': chaps})
--- web/hgbook/hgbook/urls.py
+++ web/hgbook/hgbook/urls.py
@@ -1,5 +1,6 @@
 import os, sys
 import comments.feeds as feeds
+import book.views
 from django.contrib import admin
 from django.conf.urls import include, url
 from django.contrib.syndication.views import Feed
@@ -19,7 +20,8 @@ urlpatterns = [
      {'feed_dict': feeds}),          
 
     # Only uncomment this for local testing without Apache.
-    url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^html/$', book.views.index),
     url(r'^html/(?P<path>.*)$', 'django.views.static.serve',
      {'document_root': os.path.join(settings.BASE_DIR, '..', '..', 'build', 'en', 'html')}),
     url(r'^support/(?P<path>.*)$', 'django.views.static.serve',
